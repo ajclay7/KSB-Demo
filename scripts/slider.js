b7524c22fa79f9ccf29b9534d580c765addd5420
Anthony <ajclay7@gmail.com>
2021-11-02 02:08:37 -0400

    Fix resizing issue with slick slider

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,38 +1,33 @@
-$(document).ready(function(){
-    $('.carousel').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2500,
-        dots:true,
+$('.carousel').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  mobileFirst:true,
+  autoplaySpeed: 2500,
+  dots:false,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
         arrows: false,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '4rem',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '4rem',
-              slidesToShow: 1,
-              dots: false
-            }
-          }
-        ]
-      });
-  });
-
-
-
-  
-  // Slick version 1.5.8
+        centerMode: true,
+        centerPadding: '4rem',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '4rem',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false
+      }
+    }
+  ]
+});
